8e25c6e530167cfc7b5a3c7fd7649c19f97d9cb7
Jordan Wiens <jordan@psifertex.com>
2022-01-04 21:52:14 -0500

    add default 5m timeout to status

--- slackicon.py
+++ slackicon.py
@@ -1,13 +1,16 @@
 import sys
+import time
 import pathlib
 from slack_sdk import WebClient
+import pathlib
+
 slack_token=""
-try:
-    import pathlib
-    f=open(pathlib.Path(__file__).parent.resolve() / ".token", "r")
-    slack_token=f.readlines()[0]
-    client = WebClient(token=slack_token)
-    client.users_profile_set(profile='{status_emoji:":' + sys.argv[1] + ':"}')
-except:
-    print("Unable to read token file")
-    sys.exit(-1)
+f=open(pathlib.Path(__file__).parent.resolve() / ".token", "r")
+slack_token=f.readlines()[0]
+client = WebClient(token=slack_token)
+if len(sys.argv) > 2:
+    exp = int(sys.argv[2])
+else:
+    exp = 300 # five minute expiration by default
+exp += int(time.time())
+client.users_profile_set(profile='{status_emoji:":' + sys.argv[1] + ':", status_expiration: ' + str(exp) + '}')
